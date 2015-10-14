2b6612fe0cc1a151506bb0623d4980835261aecf
amitgoelnyc <amit@expressandroid.com>
2015-10-14 00:15:28 -0400

    Added toolbar to MainActivity and implemnented onclickmenu item in nav drawers and processed navigation hamburger icon clicks to open nav drawer and close upon selection

--- app/src/main/java/com/example/tyson/retrofitexample/View/MainActivity.java
+++ app/src/main/java/com/example/tyson/retrofitexample/View/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.tyson.retrofitexample.View;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -14,11 +19,49 @@ import com.example.tyson.retrofitexample.R;
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawer;
+    View content;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        content = findViewById(R.id.content);
+        initToolbar();
+        setupDrawerLayout();
+    }
+
+    private void initToolbar() {
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupDrawerLayout() {
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Snackbar.make(content, menuItem.getTitle() + " pressed", Snackbar.LENGTH_LONG).show();
+                menuItem.setChecked(true);
+                drawer.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -33,6 +76,5 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("SearchParam", searchText);
             startActivity(intent);
         }
-
     }
 }
